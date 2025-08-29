f31c49c0da6d897a1f567f94edf9b8e07266c01e
booldook <booldook@gmail.com>
2025-08-29 15:24:18 +0900

    CSS Styled Components

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -8,8 +8,11 @@ export const ButtonWrapper = styled.div`
 
 export const Button = styled.button`
   padding: 0.5em 0.875em;
-  border-color: ${(props) => props.bColor || "#333"};
-  margin: ${(props) => props.margin || 0};
+  border-color: ${(props) => props.border || "#333"};
+  margin-left: ${(props) => props.ml || 0};
+  margin-right: ${(props) => props.mr || 0};
+  margin-top: ${(props) => props.mt || 0};
+  margin-bottom: ${(props) => props.mb || 0};
   border-style: solid;
   border-width: 1px;
   border-radius: 4px;
--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -2,7 +2,6 @@ import { css } from "@emotion/react"
 import { ButtonWrapper, Button } from "@/components/common/Button"
 
 const style = css({
-  border: "1px solid red",
   padding: "2em 0",
 })
 
@@ -10,9 +9,7 @@ export default function ChatPage() {
   return (
     <div css={style}>
       <ButtonWrapper dir="flex-end">
-        <Button bColor="red" margin="0 0.5em 0 0">
-          채팅방 참여
-        </Button>
+        <Button mr="0.5em">채팅방 참여</Button>
         <Button>채팅방 나가기</Button>
       </ButtonWrapper>
     </div>
