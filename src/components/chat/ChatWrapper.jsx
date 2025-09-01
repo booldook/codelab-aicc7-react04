247ebea2494bf683a4f31d64f3dd1b5111c28229
booldook <booldook@gmail.com>
2025-09-01 16:27:04 +0900

    chat 일단 완성

--- src/components/chat/ChatWrapper.jsx
+++ src/components/chat/ChatWrapper.jsx
@@ -41,42 +41,23 @@ const active = css`
   align-self: flex-end;
   background-color: #6ac030;
 `
-const dummy = [
-  { id: 1, name: "홍길동", comment: "가나", isMine: false },
-  { id: 2, name: "홍길만", comment: "다라", isMine: false },
-  { id: 3, name: "홍길동", comment: "마바", isMine: false },
-  { id: 4, name: "불뚝", comment: "가나다라마바사아", isMine: true },
-  { id: 5, name: "홍길순", comment: "사아", isMine: false },
-]
-
 export default function ChatWrapper({ children }) {
   const { isLogOn, user } = useSelector((state) => state.auth)
   const { rtRef, onRtValue, rtPush, rtdb } = useContext(FirebaseContext)
-  const [list, setList] = useState([...dummy])
   const [comment, setComment] = useState("")
-  const [focused, setFocused] = useState(true)
+  const [list, setList] = useState([])
   const inputRef = useRef(null)
-  const commentRef = rtRef(rtdb, "booldook")
+  const commentRef = rtRef(rtdb, "root/comment")
 
   // TODO :: 데이터를 저장, Input을 비워준다. Input에 focus
   const onClickButton = (e) => {
     if (comment.length > 0) {
-      setList((prev) => [
-        ...prev,
-        {
-          id: prev[prev.length - 1].id + 1,
-          name: "불뚝",
-          comment,
-          isMine: true,
-        },
-      ])
       rtPush(commentRef, {
-        id: user?.uid || "",
+        uid: user?.uid || "",
         name: user?.name || "",
         comment,
       })
       setComment("")
-      setFocused(true)
     }
   }
   // TODO :: shift(o) -> 아무일 안일어남. shift(x) -> 전송
@@ -89,7 +70,11 @@ export default function ChatWrapper({ children }) {
 
   useEffect(() => {
     const unsubscribe = onRtValue(commentRef, (snapshot) => {
-      console.log(snapshot?.val())
+      const dataArr = Object.values(snapshot?.val()).map((item) => {
+        item.isMine = item.uid === user.uid
+        return item
+      })
+      setList(dataArr)
     })
     return unsubscribe
   }, [])
@@ -109,7 +94,6 @@ export default function ChatWrapper({ children }) {
           onChange={(e) => setComment(e.target.value)}
           value={comment}
           autoFocus
-          focused={focused}
         />
         <Button variant="contained" onClick={onClickButton}>
           확인
