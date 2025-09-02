6f7ccdf6edd2bcff57b4312618f47a8560d3cef4
booldook <booldook@gmail.com>
2025-09-02 18:20:45 +0900

    BookList BE 연동

--- src/components/book/BookForm.jsx
+++ src/components/book/BookForm.jsx
@@ -2,6 +2,7 @@ import { useState, useContext } from "react"
 import styled from "@emotion/styled"
 import { Button } from "@mui/material"
 import { AlertContext } from "@/providers/AlertProvider"
+import axios from "axios"
 
 const FormWrap = styled.form`
   display: flex;
@@ -15,10 +16,10 @@ const Input = styled.input`
   margin-right: 0.5em;
   flex-grow: ${(props) => props.grow};
 `
-export default function BookForm() {
+export default function BookForm({ swr }) {
   const [form, setForm] = useState({ title: "", content: "" })
   const { setIsAlertOpen, setAlertMsg } = useContext(AlertContext)
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     if (form.title === "") {
       setIsAlertOpen(true)
       setAlertMsg("제목은 필수사항입니다.")
@@ -29,7 +30,12 @@ export default function BookForm() {
       setAlertMsg("내용은 필수사항입니다.")
       return
     }
-    // TODO :: 전송
+    const { data } = await axios({
+      url: import.meta.env.VITE_EXPRESS_API + "/book",
+      method: "post",
+      data: { title: form.title, content: form.content },
+    })
+    if (data?.success === "OK") swr.mutate()
   }
   return (
     <FormWrap>
