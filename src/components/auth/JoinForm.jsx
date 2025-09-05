71711113fe11c0aa57c2730543b47bb10590b090
booldook <booldook@gmail.com>
2025-09-05 16:58:00 +0900

    JoinForm UI완성

--- src/components/auth/JoinForm.jsx
+++ src/components/auth/JoinForm.jsx
@@ -1,50 +1,66 @@
-import { useState } from "react"
+import { useState, useActionState } from "react"
 import Button from "@mui/material/Button"
+import styled from "@emotion/styled"
+
+const FormWrap = styled.form`
+  padding: 0.5em;
+  border: 1px solid #eee;
+  margin-bottom: 1em;
+`
+const FormList = styled.div`
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+  padding: 0.5em 0;
+`
+const FormListTitle = styled.div`
+  font-weight: bold;
+  color: #333;
+  flex-grow: 1;
+  width: 150px;
+`
+const Input = styled.input`
+  padding: 0.75em !important;
+  border: 1px solid #aaa !important;
+  flex-grow: 3;
+`
+const ButtonWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 1em;
+`
 
 export default function JoinForm() {
-  const [search, setSearch] = useState("")
-  const onChange = (e) => {
-    setSearch(e.target.value)
-    onChangeSearch(e.target.value)
-  }
-  const onDeleteSearch = () => {
-    setSearch("")
-    onChangeSearch("")
-  }
+  const createUser = () => {}
+  const [formState, formAction] = useActionState(createUser, {})
+
   return (
     <div className="form-wrapper">
-      <form className="form-wrap">
-        <input
-          type="text"
-          className="search-input"
-          onChange={onChange}
-          value={search}
-          autoFocus
-        />
-        <span
-          className={`fa fa-times btn-delete ${search ? "active" : ""}`}
-          onClick={onDeleteSearch}
-        ></span>
-      </form>
-      <button
-        className="btn"
-        onClick={(e) => {
-          onGetPrd(e)
-          setSearch("")
-        }}
-      >
-        상품가져오기
-      </button>
-      <Button variant="contained">상품가져오기</Button>
-      <button
-        className="btn"
-        onClick={(e) => {
-          onResetPrd(e)
-          setSearch("")
-        }}
-      >
-        상품지우기
-      </button>
+      <FormWrap action={formAction}>
+        <FormList>
+          <FormListTitle>이름</FormListTitle>
+          <Input type="text" name="usrNm" />
+        </FormList>
+        <FormList>
+          <FormListTitle>아이디</FormListTitle>
+          <Input type="text" name="usrId" />
+        </FormList>
+        <FormList>
+          <FormListTitle>비밀번호</FormListTitle>
+          <Input type="password" name="usrPw" />
+        </FormList>
+        <FormList>
+          <FormListTitle>비밀번호재입력</FormListTitle>
+          <Input type="password" name="usrPwRe" />
+        </FormList>
+        <FormList>
+          <FormListTitle>이메일</FormListTitle>
+          <Input type="text" name="usrEmail" />
+        </FormList>
+        <ButtonWrap>
+          <Button variant="contained">회원가입</Button>
+        </ButtonWrap>
+      </FormWrap>
     </div>
   )
 }
--- src/pages/JoinPage.jsx
+++ src/pages/JoinPage.jsx
@@ -1,4 +1,5 @@
 import { Box, Typography } from "@mui/material"
+import JoinForm from "@/components/auth/JoinForm"
 
 export default function JoinPage() {
   return (
